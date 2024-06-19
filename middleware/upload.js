aab32263ba1b8f42353d0522b3c38e64343908bd
dugga23 <daleshruti2321@gmail.com>
2024-06-19 12:09:42 +0530

    first commit

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,6 +1,6 @@
 const multer=require('multer');
 const storage=multer.diskStorage({});
 
-const upload=multer({storage}).array('jewellery',5);
+const upload=multer({storage}).array('jewellery',10);
 
 module.exports= upload;
